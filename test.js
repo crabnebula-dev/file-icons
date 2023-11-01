d88bf9c05b7d4e87f2f6254c732c7267303da91f
Jonas Kruckenberg <118265418+CrabNejonas@users.noreply.github.com>
2023-11-01 11:03:57 +0100

    cleanup

--- test.js
+++
@@ -1,8 +0,0 @@
-import { init, getIconForFile } from './dist/lib.js'
-
-await init({
-    icons: './icons',
-    wasm: fetch(' file:////Users/crabnejonas/Documents/GitHub/file-icons/dist/file_icons.wasm'),
-})
-
-console.log(getIconForFile('test.js'))
